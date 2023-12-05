813e819633e567f43dc900f70f584e742be05e6c
rogoma007 <rogoma007@gmail.com>
2023-12-05 14:18:56 -0300

    Conexión Mysql/Pgsql en Modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    //UTILIZA CONEXIÓN A BD POSTGRESQL (LOCALHOST)
+    protected $connection = 'pgsql';
+
     /**
      * The attributes that are mass assignable.
      *
